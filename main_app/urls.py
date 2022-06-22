6362daab17db1ad771f52eb7a754790be5c2e940
marc <marcusher@outlook.com>
2022-06-22 15:32:20 +0100

    Added ListView for main index view

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('beasts/', views.beasts_index, name='index'),
-    path('cats/<int:beast_id>', views.beasts_detail, name='detail'),
+    # path('beasts/', views.beasts_index, name='index'), # Replaced with CBV ListView below
+    path('beasts/', views.BeastList.as_view(), name="index"),
+    path('beasts/<int:beast_id>', views.beasts_detail, name='detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Beast
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+# Class-Based Views
+
+class BeastList(ListView):
+    model = Beast
+
 
 # Create your views here.
 def home(request):
@@ -9,9 +17,10 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
-def beasts_index(request):
-    beasts = Beast.objects.all()
-    return render(request, 'beasts/index.html', { 'beasts': beasts })
+# Replaced with CBV above
+# def beasts_index(request):
+#     beasts = Beast.objects.all()
+#     return render(request, 'beasts/index.html', { 'beasts': beasts })
 
 def beasts_detail(request, beast_id):
     beast = Beast.objects.get(id=beast_id)
